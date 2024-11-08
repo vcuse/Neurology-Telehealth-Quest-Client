a67258a9e1bb45cf03787d5ee7bbd1dda0466663
Adam Hussein <adamhusse818@gmail.com>
2024-11-07 22:48:33 -0500

    User ids fetched successfully

---
+++ Assets/Scripts/FetchUsers.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FetchUsers : MonoBehaviour
+{
+    private string url = "https://videochat-signaling-app.ue.r.appspot.com/key=peerjs/peers";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FetchData());
+    }
+
+    IEnumerator FetchData()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            string data = request.downloadHandler.text;
+            Debug.Log("Data received: " + data);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
